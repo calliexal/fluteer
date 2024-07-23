55dfd04a19e24ae6eacf1e0b36bbd0819c8125aa
Brily Al Furqon S <159569944+calliexal@users.noreply.github.com>
2024-07-23 11:54:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
           ),
           labelStyle: TextStyle(color: sanguineBrown),
         ),
+        scaffoldBackgroundColor: whiteRock,
       ),
       initialRoute: '/',
       routes: {
